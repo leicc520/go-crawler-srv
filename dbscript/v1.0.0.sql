c498eb46700dc7c573eeb58c5f2b4196524262a4
zhuzubin <zubin_zhu@fzzixun.com>
2022-09-16 15:45:49 +0800

    新增升级脚本

---
+++ dbscript/v1.0.0.sql
@@ -0,0 +1,26 @@
+create table sys_activation_code
+(
+    id            serial primary key,
+    status        boolean default false,
+    code          varchar(64),
+    activate_time timestamp,
+    create_time   timestamp,
+    expire_time   timestamp
+);
+
+comment on column sys_activation_code.status is '是否激活 默认未激活';
+
+comment on column sys_activation_code.code is '激活码';
+
+comment on column sys_activation_code.activate_time is '激活时间';
+
+comment on column sys_activation_code.create_time is '激活码生成时间';
+
+comment on column sys_activation_code.expire_time is '过期时间';
+
+
+
+create sequence sys_activation_code_id_seq as integer;
+
+alter sequence sys_activation_code_id_seq owned by sys_activation_code.id;
+
